8c849b807310046e463eacee28b07f04b45113b0
Yahyazawadi <yahyazawadi278@gmail.com>
2025-12-04 00:58:36 +0200

    added test unit file automatically via intellj

---
+++ src/main/java/ProductStockTest.java
@@ -0,0 +1,82 @@
+package main.java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductStockTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getProductId() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getLocation() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getOnHand() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getReserved() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAvailable() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getReorderThreshold() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getMaxCapacity() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void changeLocation() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addStock() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeDamaged() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void reserve() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void releaseReservation() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void shipReserved() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isReorderNeeded() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void updateReorderThreshold() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void updateMaxCapacity() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testToString() {
+    }
+}
